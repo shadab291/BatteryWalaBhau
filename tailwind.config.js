dc0332c9705401548c9f8edecb03aabd8258a69a
shadab291 <70841658+shadab291@users.noreply.github.com>
2024-05-25 11:19:55 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your project's structure
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4A90E2', // Adjust to your primary color
+        secondary: '#FEB65E', // Adjust to your secondary color
+        accent: '#6D28D9', // Adjust to your accent color
+      },
+      keyframes: {
+        revealText: {
+          '0%': { opacity: 0, transform: 'translateY(100%)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        revealText: 'revealText 1s ease-in-out forwards',
+      },
+    },
   },
   plugins: [],
-}
+};
